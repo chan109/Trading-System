d9508c542cf56dc89ada1d2908fb9f5f314c7347
Michael Chen <749827352@qq.com>
2016-12-03 19:48:10 -0800

    submit

--- statics/js/app.js
+++ statics/js/app.js
@@ -1,8 +1,9 @@
 var myapp = angular.module("warren", ["zingchart-angularjs"]);
-var host = "http://gabrieluribe.me:5000/stocks/all";
+var host = "http://gabrieluribe.me:5000/";
 var buyHost = "http://gabrieluribe.me:5000/stocks/buy";
+var sellHost = "http://gabrieluribe.me:5000/stocks/sell";
 var testHost = "http://localhost:5000/stocks/";
-var test = "http://localhost:5000/stocks/all";
+var test = "http://localhost:5000/stocks/";
 myapp.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 }]);
@@ -15,10 +16,10 @@ myapp.controller("mainController", function ($scope, $http, $interval) {
 
     //start polling data from the service
     $scope.myData = [1,2,28,4,5];
-
-
+    $scope.money=10000;
+    $scope.test="hello";
     $http({
-        url: test,
+        url: test+"all",
         method: "GET"
     }).then(function successCallback(response) {
         console.log(response.data);
@@ -40,7 +41,40 @@ myapp.controller("mainController", function ($scope, $http, $interval) {
         }).then(function successCallback(response) {
             console.log(response.data);
             debugger
-                $scope.stocks=response.data;
+                $scope.money=response.data;
+            // this callbalsck will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            console.log(response);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
+    $scope.sell = function (symbol) {
+
+        $http({
+            url: sellHost,
+            method: "POST",
+            data:"symbol="+symbol
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            debugger
+            $scope.money=response.data;
+            // this callbalsck will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            console.log(response);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
+    $scope.proforio=function () {
+        $http({
+            url: host+"user/portfolio",
+            method: "GET"
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            $scope.stocks=response.data;
             // this callbalsck will be called asynchronously
             // when the response is available
         }, function errorCallback(response) {
@@ -49,4 +83,21 @@ myapp.controller("mainController", function ($scope, $http, $interval) {
             // or server returns response with an error status.
         });
     }
+    
+    $scope.home=function () {
+        $http({
+            url: test+"all",
+            method: "GET"
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            $scope.stocks=response.data;
+            // this callbalsck will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            console.log(response);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
 });
